1159c15b3752be59ebb1f014adcbf37e47148f28
Arnav Gandhi <gandhiarnav10000@gmail.com>
2026-07-28 19:49:55 +0530

    Add: sql-customers-bought-a-b-not-c - submission-2

---
+++ SQL/sql-customers-bought-a-b-not-c/submission-2.sql
@@ -0,0 +1,18 @@
+select customer_id, customer_name
+from customers where customer_id in(
+    select distinct customer_id
+    from orders
+    where customer_id in (
+    select distinct customer_id from orders
+    where product_name = 'A'
+    )
+    and customer_id in (
+    select distinct customer_id from orders
+    where product_name = 'B'
+    )
+    and customer_id not in (
+    select distinct customer_id from orders
+    where product_name = 'C'
+    )
+)
+order by customer_name 
