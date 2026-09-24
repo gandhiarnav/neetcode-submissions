78d93c95bb2ecdae43cc6b1b845752606c505b69
Arnav Gandhi <gandhiarnav10000@gmail.com>
2026-09-24 19:11:01 +0530

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.py
@@ -0,0 +1,7 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        ds = Counter(s)
+        dt = Counter(t)
+        if ds == dt:
+            return True
+        return False
